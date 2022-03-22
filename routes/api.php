ad691bcac945dece4806f7b9e237b1213e287516
ujez <ujezujez@gmail.com>
2022-03-22 15:19:17 +0100

    the format of the resource was defined in the api.php here

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
@@ -19,5 +20,7 @@ Route::middleware('auth:api')->prefix('v1')->group(function(){
     Route::get('/user', function(Request $request){
         return $request->user();
     });
+
+    Route::get('/authors/{author}', [AuthorsController::class, 'show']);
     
 });
